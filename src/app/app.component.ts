e5ff06272538996f38c39e9cb4554c00ed559709
AlixJo <alix-jourdan@hotmail.fr>
2018-09-19 14:51:28 +0200

    Modif Alix Erreur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './message.component.html',
-  styleUrls: ['./message.component.css']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app works!';
