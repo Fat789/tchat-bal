11bc4f772291795c393e4fdb06877a7eec94d87d
AlixJo <alix-jourdan@hotmail.fr>
2018-09-19 14:54:03 +0200

    Modif Alix ERREUR 2

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -13,3 +13,20 @@ export class MessageComponent implements OnInit {
   }
 
 }
+
+
+export class Message {
+  id: number;
+  version: number;
+  texte: string;
+  dtExpedition: Date;
+  expediteur: any ;
+
+  constructor(id: number, version: number, texte: string, dtExpedition: Date, expediteur: any) {
+    this.id = id;
+    this.version = version;
+    this.texte = texte;
+    this.dtExpedition = dtExpedition;
+    this.expediteur = expediteur;
+  }
+}
