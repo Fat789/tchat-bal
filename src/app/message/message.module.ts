69caa66134b7baaa852ef2b5c79f2916995be835
AlixJo <alix-jourdan@hotmail.fr>
2018-09-19 16:04:22 +0200

    Modif Message

--- src/app/message/message.module.ts
+++ src/app/message/message.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MessageComponent} from './message.component';
+import {RouterModule, Routes} from '@angular/router';
 
+
+const routes: Routes = [
+  {path: 'message', component: MessageComponent}
+]
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
   ],
   declarations: [MessageComponent]
 })
