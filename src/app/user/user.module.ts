00af1223ef5e8d472231557fbd1f99dfffbd3f17
Fat <baptiste.6@hotmail.com>
2018-09-19 17:07:45 +0200

    modif sur module user

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {UserComponent} from './user.component';
-import {Routes} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
   {path: 'utilisateur', component: UserComponent},
 ];
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
   declarations: [
     UserComponent,
   ]
+
 })
 export class UserModule { }
