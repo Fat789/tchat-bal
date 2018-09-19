31fbd39b78f2e33f34b15f2985df749baf0966af
Systank <lucas.crappeel@laposte.net>
2018-09-19 15:44:09 +0200

    Ajout d'un / après Api dans l'app config

--- src/app/salon/salon.component.ts
+++ src/app/salon/salon.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {SalonService} from './salon.service';
 
 @Component({
   selector: 'app-salon',
@@ -6,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SalonComponent implements OnInit {
 
-  constructor() { }
+  constructor(private salonService: SalonService) { }
 
   ngOnInit() {
   }
+  public list() {
+    return this.salonService.findAll();
+  }
 
+  public detail(id:  number){
+    return this.salonService.findById(id);
+  }
 }
